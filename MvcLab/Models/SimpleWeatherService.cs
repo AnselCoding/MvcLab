5f62afd16ad0fb959b5ee15a10687b1fee432139
Ansel90095 <anselsiao@gmail.com>
2023-08-11 15:31:14 +0800

    Comment GetTaipeiWeatherFromOpenDataApi. Use GetWeatherFromOpenDataApi.

--- MvcLab/Models/SimpleWeatherService.cs
+++ MvcLab/Models/SimpleWeatherService.cs
@@ -27,37 +27,39 @@ namespace MvcLab.Models
             _configuration = configuration;
         }
 
-        public WeatherData GetTaipeiWeatherFromOpenDataApi()
-        {
-            //TODO 應改為 async/await
-            var json = _callAPI.Get(_configuration["openDataApiUrl"]);
-            using (var doc = JsonDocument.Parse(json,
-                new JsonDocumentOptions { AllowTrailingCommas = true }))
-            {
-                var taipeiData = doc
-                    .RootElement
-                    .GetProperty("records")
-                    .GetProperty("location")
-                    .EnumerateArray()
-                    .Single(o => o.GetProperty("locationName").GetString() == "臺北市")
-                    .GetProperty("weatherElement")
-                    .EnumerateArray();
+        //ViewComponents Test1
+        //public WeatherData GetTaipeiWeatherFromOpenDataApi()
+        //{
+        //    //TODO 應改為 async/await
+        //    var json = _callAPI.Get(_configuration["openDataApiUrl"]);
+        //    using (var doc = JsonDocument.Parse(json,
+        //        new JsonDocumentOptions { AllowTrailingCommas = true }))
+        //    {
+        //        var taipeiData = doc
+        //            .RootElement
+        //            .GetProperty("records")
+        //            .GetProperty("location")
+        //            .EnumerateArray()
+        //            .Single(o => o.GetProperty("locationName").GetString() == "臺北市")
+        //            .GetProperty("weatherElement")
+        //            .EnumerateArray();
 
-                Func<string, string> readParameterName =
-                    (elemName) =>
-                    taipeiData.Single(o => o.GetProperty("elementName").GetString() == elemName)
-                        .GetProperty("time").EnumerateArray().First()
-                        .GetProperty("parameter").GetProperty("parameterName").GetString();
+        //        Func<string, string> readParameterName =
+        //            (elemName) =>
+        //            taipeiData.Single(o => o.GetProperty("elementName").GetString() == elemName)
+        //                .GetProperty("time").EnumerateArray().First()
+        //                .GetProperty("parameter").GetProperty("parameterName").GetString();
 
-                return new WeatherData
-                {
-                    Status = readParameterName("Wx"),
-                    MaxTemp = readParameterName("MaxT"),
-                    MinTemp = readParameterName("MinT")
-                };
-            }
-        }
+        //        return new WeatherData
+        //        {
+        //            Status = readParameterName("Wx"),
+        //            MaxTemp = readParameterName("MaxT"),
+        //            MinTemp = readParameterName("MinT")
+        //        };
+        //    }
+        //}
 
+        //ViewComponents Test2
         public WeatherData GetWeatherFromOpenDataApi(string zoneName)
         {
             var json = _callAPI.Get(_configuration["openDataApiUrl"]);
--- MvcLab/ViewComponents/TaipeiWeatherViewComponent.cs
+++ MvcLab/ViewComponents/TaipeiWeatherViewComponent.cs
@@ -3,20 +3,21 @@ using MvcLab.NetTool;
 
 namespace MvcLab.ViewComponents
 {
-    public class TaipeiWeatherViewComponent
-    {
-        private readonly IConfiguration _configuration;
-        public TaipeiWeatherViewComponent(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-        public string Invoke()
-        {
-            //TODO: 應改用 DI https://blog.darkthread.net/blog/aspnet-core-di-notes/
-            var svc = new SimpleWeatherService(_configuration);
-            var data = svc.GetTaipeiWeatherFromOpenDataApi();
+    // ViewComponents Test1
+    //public class TaipeiWeatherViewComponent
+    //{
+    //    private readonly IConfiguration _configuration;
+    //    public TaipeiWeatherViewComponent(IConfiguration configuration)
+    //    {
+    //        _configuration = configuration;
+    //    }
+    //    public string Invoke()
+    //    {
+    //        //TODO: 應改用 DI https://blog.darkthread.net/blog/aspnet-core-di-notes/
+    //        var svc = new SimpleWeatherService(_configuration);
+    //        var data = svc.GetTaipeiWeatherFromOpenDataApi();
 
-            return $"現在台北天氣：{data.Status} /氣溫：{data.MinTemp}° - {data.MaxTemp}°";
-        }
-    }
+    //        return $"現在台北天氣：{data.Status} /氣溫：{data.MinTemp}° - {data.MaxTemp}°";
+    //    }
+    //}
 }
